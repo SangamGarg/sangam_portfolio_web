8162eb3aaffa63ac9057e68aa653dc6bb8fda301
SangamGarg <sangamgarg17@gmail.com>
2026-02-02 00:23:22 +0530

    Resume Update

--- src/main/java/sangamportfolioweb/sangamportfolioweb/enums/experienceEnum/WorkDetails.java
+++ src/main/java/sangamportfolioweb/sangamportfolioweb/enums/experienceEnum/WorkDetails.java
@@ -5,14 +5,14 @@ import lombok.Getter;
 @Getter
 public enum WorkDetails {
 
-    ANDROID_DEVELOPER_INTERN_DETAILS("•\tExcelled as an Android Developer at Talen Titan Private Limited, enhancing app functionality by integrating external data sources and REST APIs, and optimizing data management with ROOM database, improving data processing efficiency by 30%. Successfully implemented ads in Android applications, contributing to a 15% increase in revenue.\n" +
-            "•\tResolved software bugs and performance issues, optimized code, debugging code and ensured high-quality performance, delivering interactive and user-friendly interfaces.\n" +
-            "•\tLeveraged modern Android technologies such as MVVM, Retrofit, Kotlin Coroutines, and Kotlin Flows, reducing development time by 20% and ensuring seamless app scalability.\n"),
+    ANDROID_DEVELOPER_INTERN_DETAILS("• Developed and enhanced Android app features by integrating external data sources and RESTful APIs, optimizing data management with in memory database, leading to a 30% improvement in data processing efficiency.\n" +
+            "• Implemented in-app ads that contributed to a 15% increase in app-generated revenue.\n" +
+            "• Diagnosed and resolved software bugs, performance issues, and performed debugging to ensure high-quality performance, delivering interactive and user-friendly interfaces."),
 
 
-    JAVA_DEVELOPER_DETAILS("•\tContributed to the Branch Portal module where bank branches upload customer documents for verification and approval workflows.\n" +
-            "•\tImplemented SQL query optimization and indexing strategies that reduced data retrieval time by over 25%, improving the performance of high-volume extraction jobs.\n" +
-            "•\tContributed to redundant code optimization and modularization, reducing maintenance overhead and improving code reusability across multiple microservices.\n");
+    JAVA_DEVELOPER_DETAILS("• Optimized customer account lookups, reducing repeated server calls by 40% and improving average response time by 30% during branch customer verification workflows.\n" +
+            "• Implemented SQL query optimization and indexing strategies that reduced data retrieval time by over 25%, improving the performance of high-volume extraction jobs.\n" +
+            "• Contributed to redundant");
 
     private final String value;
 
